9995d83646b8d2b6ac285d92801350e9c9150fc0
Shivakumar K S <ce24d033@smail.iitm.ac.in>
2026-04-29 15:36:29 +0530

    updated

--- psb_MatlabProcessors/sks_ExtractBeamAndColumnPHR.m
+++ psb_MatlabProcessors/sks_ExtractBeamAndColumnPHR.m
@@ -1,6 +1,7 @@
 clear; clc;
 
 
+
 baseDir = 'E:\OpenSees_PracticeExamples\ida-spo-msa\Output\(ID2433_R5_5Story_v.02)_(AllVar)_(0.00)_(clough)';
 
 %% Get EQ folders
@@ -10,31 +11,32 @@ eqFolders = eqFolders([eqFolders.isdir]);
 %% storage
 columnPHR = struct();
 beamPHR   = struct();
-
+periodUsedForScalingGroundMotions = struct();
 for iEQ = 1:length(eqFolders)
-    eqName = eqFolders(iEQ).name;
-    eqPath = fullfile(baseDir, eqName);
+    eqNumber = eqFolders(iEQ).name;
+    eqPath = fullfile(baseDir, eqNumber);
     saFolders = dir(fullfile(eqPath,'Sa_*'));
     saFolders = saFolders([saFolders.isdir]);
 
     for iSa = 1:length(saFolders)
-        saName = saFolders(iSa).name;
-        saPath = fullfile(eqPath, saName);
-        saField = strrep(saName,'.','p');
+        saVal = saFolders(iSa).name;
+        saPath = fullfile(eqPath, saVal);
+        saField = strrep(saVal,'.','p');
         matFile = fullfile(saPath,'DATA_reducedSensDataForThisSingleRun.mat');
 
         if ~isfile(matFile)
             continue
         end
 
-        S = load(matFile,'columnAbsMaxPHRPerStory','beamAbsMaxPHRPerFloor');
-        columnPHR.(eqName).(saField) = S.columnAbsMaxPHRPerStory;
-        beamPHR.(eqName).(saField)   = S.beamAbsMaxPHRPerFloor;
+        PHRData = load(matFile,'columnAbsMaxPHRPerStory','beamAbsMaxPHRPerFloor', 'periodUsedForScalingGroundMotionsFromMatlab');
+        columnPHR.(eqNumber).(saField) = PHRData.columnAbsMaxPHRPerStory;
+        beamPHR.(eqNumber).(saField)   = PHRData.beamAbsMaxPHRPerFloor;
+        periodUsedForScalingGroundMotions.(eqNumber).(saField) = PHRData.periodUsedForScalingGroundMotionsFromMatlab;
 
     end
 end
 
 %% save
-save(fullfile(baseDir,'DATA_BeamAndColumn_PHR_allEQ.mat'),'columnPHR','beamPHR','-v7.3');
+save(fullfile(baseDir,'DATA_BeamAndColumn_PHR_allEQ.mat'),'columnPHR','beamPHR', 'periodUsedForScalingGroundMotions','-v7.3');
 
 
